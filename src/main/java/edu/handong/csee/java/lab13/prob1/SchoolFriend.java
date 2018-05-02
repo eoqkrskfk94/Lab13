7fa89c98d8298c18bbe6f26f524e1840950e0f87
Kim Myung Jin <eoqkrskfk94@naver.com>
2018-05-02 22:03:58 +0900

    SchoolFriend class

---
+++ src/main/java/edu/handong/csee/java/lab13/prob1/SchoolFriend.java
@@ -0,0 +1,16 @@
+package edu.handong.csee.java.lab13.prob1; //package name
+/**
+ * This class inherits Friend class<br>
+ * 
+ * This class has the follwing method:<br>
+ * schoolFriend(): prints of info<br>
+ * 
+ * @author Kim Myung Jin 21400082
+ *
+ */
+public class SchoolFriend extends Friend{ // class SchoolFriend inherits class Friend
+
+	public void schoolFriend() { //declaration of schoolFriend method
+		System.out.println("School Friend."); //prints of the info
+	}
+}
