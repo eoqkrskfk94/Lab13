084792b1753d7fb45ff9ff47e8b2864e201e9709
Kim Myung Jin <eoqkrskfk94@naver.com>
2018-05-02 23:30:34 +0900

    new edit

--- src/main/java/edu/handong/csee/java/lab13/prob6/UpPoint.java
+++ src/main/java/edu/handong/csee/java/lab13/prob6/UpPoint.java
@@ -1,19 +1,23 @@
-package edu.handong.csee.java.lab13.prob6;
+package edu.handong.csee.java.lab13.prob6;//package name
 
-public class UpPoint implements CapitalPrint{
-	private int a, b;
+public class UpPoint implements CapitalPrint{//declaration of class UpPoint that implements CapitalPrint class
+	private int a, b;//declaration of private integer variables a and b
 
-	public UpPoint(int a, int b){
+	public UpPoint(int a, int b){//declaration of constructor method with the parameters of a and b
 
-		this.a = a;
-		this.b = b;
+		this.a = a;//insert the value of parameter a into a of this class
+		this.b = b;//insert the value of parameter b into a of this class
 	}
 
-	public String toString() {
+	public String toString() {//declaration of toString method
 
-		return "x: " + a + " y: " + b;
+		return "x: " + a + " y: " + b;//returns the give string value  
 	}
 
 
 
 }
+
+
+
+
