953fbbad903af66086e4750f93a747de57cafddf
Kim Myung Jin <eoqkrskfk94@naver.com>
2018-05-02 22:03:15 +0900

    Friend class

---
+++ src/main/java/edu/handong/csee/java/lab13/prob1/Friend.java
@@ -0,0 +1,16 @@
+package edu.handong.csee.java.lab13.prob1; //package name
+/**
+ * 
+ * 
+ * This class has the follwing method:<br>
+ * justFriend(): prints of info<br>
+ * 
+ * @author Kim Myung Jin 21400082
+ *
+ */
+public class Friend { //declaration of class Friend (Superclass)
+
+	public void justFriend(){ // declaration of justFriend method
+		System.out.println("Just Friend."); //prints out the info
+	}
+}
